8a0d33284e98734001f433889ff54ee445ec7c5d
Ruxton <ruxton@thefrontiergroup.com.au>
2014-10-20 20:50:58 +0800

    Update run_me to replace files in new_path and remove git-remote origin

--- run_me.rb
+++ run_me.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/ruby
+
 require 'active_support/inflector'
 include ActiveSupport::Inflector
 
@@ -5,8 +7,8 @@ def heading(heading)
   puts("#{heading}\n")
 end
 
-def replace(path, original, with)
-  run_command("find . -type f -name '#{path}' -exec sed -i '' s/#{original.chomp}/#{with.chomp}/ {} +")
+def replace(path, match, original, with)
+  run_command("find #{path} -type f -name '#{match}' -exec sed -i '' s/#{original.chomp}/#{with.chomp}/ {} +")
 end
 
 def run_command(command)
@@ -21,14 +23,9 @@ def prompt(string,default=nil)
   result.empty? ? default : result
 end
 
-# github_repo = prompt "What is the github repo for the new app?"
-# heading("Changing the git origin to #{github_repo}")
-# run_command("git remote rm origin")
-# run_command("git remote add origin #{github_repo}")
-
 app_name = prompt "What is the name of the new app? (EG: App Name)"
 
-target_path = prompt "Where should we install too?", "../"
+target_path = prompt "Where should we install to?", "../"
 
 new_path = File.join(target_path,app_name.parameterize("_"))
 
@@ -36,11 +33,19 @@ heading("Cloning to #{new_path}")
 run_command("git clone . #{new_path}")
 
 heading("Changing all instances referencing template name to appropriate casing of #{app_name}")
-replace(File.join(new_path,"*application.rb"), "TfgTemplate", app_name)
-replace(File.join(new_path,"*application.html.haml"), "TfgTemplate", app_name)
-replace(File.join(new_path,"*.ruby-gemset"), "tfg_template", app_name.parameterize("_"))
-replace(File.join(new_path,"*database.yml"), "tfg-template", app_name.parameterize("-"))
-replace(File.join(new_path,"*APP_README.md"), "TfgTemplate", app_name)
+replace(new_path,"*application.rb", "TfgTemplate", app_name)
+replace(new_path,"*application.html.haml", "TfgTemplate", app_name)
+replace(new_path,"*.ruby-gemset", "tfg_template", app_name.parameterize("_"))
+replace(new_path,"*database.yml", "tfg-template", app_name.parameterize("-"))
+replace(new_path,"*APP_README.md", "TfgTemplate", app_name)
 
 heading("Overwriting existing README.md with template for a Rails app's README")
 run_command("cd #{new_path} && mv APP_README.md README.md && cd -")
+
+heading("Comitting changes to your new app at #{new_path}")
+run_command("cd #{new_path} && git remote rm origin && git add . && git commit -m '[rails-template] CONFIGURED'")
+
+puts
+puts
+puts "\e[1;32;40mYour new app is configured at #{new_path}\e[0m"
+puts
