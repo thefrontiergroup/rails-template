eb03c8f54c61b7cacde60cdddf5644606697b6eb
Ruxton <ruxton@thefrontiergroup.com.au>
2014-10-20 20:10:57 +0800

    Add rspec_junit_formatter + rcov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,12 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
+require 'simplecov'
+require 'simplecov-rcov'
+
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+SimpleCov.start 'rails'
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
