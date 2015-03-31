dde5143f9772be8b698a6357811379627f1a4ab0
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2015-03-31 16:29:00 +0800

    Normalize app name

--- run_me.rb
+++ run_me.rb
@@ -7,6 +7,19 @@ def heading(heading)
   puts("#{heading}\n")
 end
 
+# app_name can be:
+#   * One Two
+#   * one-two
+#   * one two
+#   * one.two
+#   * one_two
+# These should all come out as: One Two
+def normalize_app_name(app_name)
+  # . or - or ' ' or _
+  regexp = /\.|-|\s|_/
+  app_name.titleize.gsub(/\.|-|\s|_/, " ")
+end
+
 def replace(path, match, original, with)
   run_command("find #{path} -type f -name '#{match}' -exec sed -i '' s/#{original.chomp}/#{with.chomp}/ {} +")
 end
@@ -23,7 +36,8 @@ def prompt(string,default=nil)
   result.empty? ? default : result
 end
 
-app_name = prompt "What is the name of the new app? (EG: App Name)"
+app_name = normalize_app_name(prompt("What is the name of the new app? (EG: App Name)"))
+app_name_as_constant = app_name.gsub(" ", "")
 
 target_path = prompt "Where should we install to?", "../"
 
@@ -33,11 +47,11 @@ heading("Cloning to #{new_path}")
 run_command("git clone . #{new_path}")
 
 heading("Changing all instances referencing template name to appropriate casing of #{app_name}")
-replace(new_path,"*application.rb", "TfgTemplate", app_name)
-replace(new_path,"*application.html.haml", "TfgTemplate", app_name)
-replace(new_path,"*.ruby-gemset", "tfg_template", app_name.parameterize("_"))
-replace(new_path,"*database.yml", "tfg-template", app_name.parameterize("-"))
-replace(new_path,"*APP_README.md", "TfgTemplate", app_name)
+replace(new_path, "*application.rb", "TfgTemplate", app_name_as_constant)
+replace(new_path, "*application.html.haml", "TfgTemplate", app_name_as_constant)
+replace(new_path, "*.ruby-gemset", "tfg_template", app_name.parameterize("_"))
+replace(new_path, "*database.yml", "tfg-template", app_name.parameterize("-"))
+replace(new_path, "*APP_README.md", "TfgTemplate", app_name)
 
 heading("Overwriting existing README.md with template for a Rails app's README")
 run_command("cd #{new_path} && mv APP_README.md README.md && cd -")
