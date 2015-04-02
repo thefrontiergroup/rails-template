4c65f6fdb823e68d5901317c446722bb56de2fa4
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2015-04-02 13:34:04 +0800

    Ensure users are sent to their corresponding dashboards

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,20 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+protected
+
+  # Provided by Devise
+  def after_sign_in_path_for(resource)
+    user_policy = UserPolicy.new(resource, resource)
+    if user_policy.admin_dashboard?
+      admin_dashboard_index_path
+    elsif user_policy.member_dashboard?
+      member_dashboard_index_path
+    else
+      root_path
+    end
+  end
+
 private
 
   def user_not_authorized
