d1c600be7f663182136813374aec742c59a57a47
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2015-04-09 22:46:50 +0800

    Schema updates

--- db/schema.rb
+++ db/schema.rb
@@ -17,19 +17,19 @@ ActiveRecord::Schema.define(version: 20150402035454) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "role",                                null: false
+    t.integer  "role",                                            null: false
     t.datetime "deleted_at"
   end
 
