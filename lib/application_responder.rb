253802889f0a1d8f70bb18ce685bd14a54d03380
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2015-05-29 11:59:52 +0800

    Use collection responder

--- lib/application_responder.rb
+++ lib/application_responder.rb
@@ -4,5 +4,5 @@ class ApplicationResponder < ActionController::Responder
 
   # Redirects resources to the collection path (index action) instead
   # of the resource path (show action) for POST/PUT/DELETE requests.
-  # include Responders::CollectionResponder
+  include Responders::CollectionResponder
 end
