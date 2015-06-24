1fe3ef1b90aecb2c81a0448c6738813eb5a12910
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2015-06-24 12:08:25 +0800

    Do not conditionally compile assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,8 +1,8 @@
+# No need to conditionally compile assets - if we compile an asset and don't use it, it won't
+# be served anyway.
+#
 Rails.application.config.assets.precompile += %w(
   admin.css
+  easy_sign_in.js
   public.css
 )
-
-if Rails.application.config.should_show_easy_login
-  Rails.application.config.assets.precompile += %w(easy_sign_in.js)
-end
