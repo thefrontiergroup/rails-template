4c34dfbc3570df159d2f4e1a59c51aa8769017e8
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2015-07-02 16:35:18 +0800

    Use indifferent access

--- spec/support/controller_parameter_support.rb
+++ spec/support/controller_parameter_support.rb
@@ -3,8 +3,8 @@ module ControllerParameterSupport
   # FactoryGirl.attributes_for ignores associations, which is problematic for us. So we make
   # our own implementation.
   def parameters_for(*factory_args)
-    all_attributes = FactoryGirl.build(*factory_args).attributes
-    all_attributes = all_attributes.reject {|attr_name| attr_name.to_s.in?(["id", "created_at", "updated_at", "deleted_at"]) }
+    all_attributes = FactoryGirl.build(*factory_args).attributes.with_indifferent_access
+    all_attributes = all_attributes.reject {|attr_name| attr_name.in?(["id", "created_at", "updated_at", "deleted_at"]) }
     all_attributes
   end
 
