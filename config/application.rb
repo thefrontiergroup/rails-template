bc2fe38eeab8af5698f314476f1eb10df05af4c8
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2015-07-03 16:09:46 +0800

    Update name

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RailsTemplate
+module TfgTemplate
   class Application < Rails::Application
     # Use the responders controller from the responders gem
     config.app_generators.scaffold_controller :responders_controller
