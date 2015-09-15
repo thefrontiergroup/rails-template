49a79db310abdc6f89a49c610f39965e0281833b
Vinicius Osiro <vinny@thefrontiergroup.com.au>
2015-09-15 10:42:54 +0800

    Fixes from code review

--- spec/support/controller_parameter_support.rb
+++ spec/support/controller_parameter_support.rb
@@ -3,16 +3,19 @@ module ControllerParameterSupport
   # FactoryGirl.attributes_for ignores associations, which is problematic for us. So we make
   # our own implementation.
   def parameters_for(*factory_args)
-    all_attributes = sanitize_attributes(FactoryGirl.build(*factory_args))
-    all_attributes = all_attributes.reject {|attr_name| attr_name.in?(["id", "created_at", "updated_at", "deleted_at"]) }
-    all_attributes
+    sanitize_attributes(FactoryGirl.build(*factory_args))
   end
 
 private
 
   def sanitize_attributes(instance)
-    attributes = instance.attributes
+    attributes = sanitize_enums(instance, instance.attributes)
+    attributes
+      .reject {|attr_name| attr_name.in?(["id", "created_at", "updated_at", "deleted_at"]) }
+      .with_indifferent_access
+  end
 
+  def sanitize_enums(instance, attributes)
     # Replaces enums integers with strings.
     # E.G:
     # instance = User.last
@@ -23,6 +26,6 @@ private
       attributes[enum] = values.key(attributes[enum])
     end
 
-    attributes.with_indifferent_access
+    attributes
   end
 end
