be2e52bc97053821f95d1adbba3e1a7e240b5408
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2015-10-21 16:59:39 +0800

    Refactor: Move after sign in path into own class

---
+++ spec/lib/after_sign_in_path_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe AfterSignInPath do
+  include Rails.application.routes.url_helpers
+
+  let(:path) { AfterSignInPath.new(user) }
+
+  describe "#to_s" do
+    subject { path.to_s }
+
+    context "when admin" do
+      let(:user) { FactoryGirl.build(:user, :admin) }
+      it { should eq(admin_dashboard_index_path) }
+    end
+
+    context "when member" do
+      let(:user) { FactoryGirl.build(:user, :member) }
+      it { should eq(member_dashboard_index_path) }
+    end
+
+    context "when nil" do
+      let(:user) { nil }
+      it { should eq(root_path) }
+    end
+  end
+
+end
