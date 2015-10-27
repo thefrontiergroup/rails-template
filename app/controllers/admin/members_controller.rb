5aeb401517dfda329d68f48e362b3b9813bb4647
JK Gunnink <jgunnink@gmail.com>
2015-10-27 11:12:28 +0800

    refactored the admin and members controllers to reference the users controller

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,53 +1,9 @@
-class Admin::MembersController < Admin::BaseController
-
-  def index
-    authorize(User)
-    @users = sort(policy_scope(User.member)).page(params[:page])
+class Admin::MembersController < Admin::UsersController
+  def user_role
+    "member"
   end
 
-  def new
-    @user = User.new
-    authorize(@user)
+  def redirect_path
+    admin_members_path
   end
-
-  def create
-    @user = User.member.new
-    authorize(@user)
-    @user.update_attributes(user_form_attributes(@user))
-
-    respond_with(@user, location: admin_members_path)
-  end
-
-  def edit
-    @user = find_user
-    authorize(@user)
-  end
-
-  def update
-    @user = find_user
-    authorize(@user)
-    @user.update_attributes(user_form_attributes(@user))
-
-    respond_with(@user, location: admin_members_path)
-  end
-
-  def destroy
-    @user = find_user
-    authorize(@user)
-    @user.destroy
-    redirect_to(admin_members_path, notice: "'#{@user}' deleted")
-  end
-
-private
-
-  def find_user
-    User.find(params[:id])
-  end
-
-  def user_form_attributes(user)
-    params.require(:user).permit(policy(user).permitted_attributes).tap do |attributes|
-      attributes[:user].delete(:password) if attributes[:user].present? && attributes[:user][:password].blank?
-    end
-  end
-
 end
