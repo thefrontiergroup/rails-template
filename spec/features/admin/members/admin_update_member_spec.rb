a71eef25c04a7f7de5ff547e18e764960c24d91a
JK Gunnink <jgunnink@gmail.com>
2015-11-12 11:35:26 +0800

    Fixed a redirecting issue

--- spec/features/admin/members/admin_update_member_spec.rb
+++ spec/features/admin/members/admin_update_member_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'Admin can update an existing User' do
 
   signed_in_as(:admin) do
-    let!(:target_user) { FactoryGirl.create(:user, email: "something@nothing.com") }
+    let!(:target_user) { FactoryGirl.create(:user, :member, email: "something@nothing.com") }
 
     before do
       click_header_option("Dashboard")
