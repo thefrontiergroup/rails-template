a31cba0065a39ec1a27ee42f574ca203edb21cb3
JK Gunnink <jk@thefrontiergroup.com.au>
2016-01-07 10:32:48 +0800

    Updated authorization specs and policy scope in tests for better clarity and understanding

--- spec/authorization/policies/application_policy_spec.rb
+++ spec/authorization/policies/application_policy_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe ApplicationPolicy do
   subject { policy }
-  let(:policy) { ApplicationPolicy.new(user, target_user) }
-  let(:target_user) { user }
+  let(:policy) { ApplicationPolicy.new(user, unused_object) }
+  #target object is not used in the policy, however pundit requires two objects to be passed in
+  let(:unused_object) { "unsed object" }
 
   context "for an anonymous user" do
     let(:user) { nil }
@@ -36,6 +37,7 @@ RSpec.describe ApplicationPolicy do
     it { should_not permit_access_to(:index) }
     it { should_not permit_access_to(:new) }
     it { should_not permit_access_to(:create) }
+    it { should_not permit_access_to(:edit) }
     it { should_not permit_access_to(:update) }
     it { should_not permit_access_to(:destroy) }
   end
