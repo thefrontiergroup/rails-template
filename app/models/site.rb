88aec7f4e53664be1f92cee1021668c41d8f4a1b
TFG <tfg@tfgs-imac.thefrontiergroup.net.au>
2016-01-07 09:35:36 +0800

    Abstracted searching for a site by name in the site class

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,2 +1,5 @@
 class Site < ActiveRecord::Base
+
+  scope :named, -> (site_name) { where("sites.name ILIKE ?", "%#{site_name}%") }
+
 end
--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Site, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe ".named", :focus do
+    subject { Site.named("xyz") }
+
+    let!(:site_that_matches_exactly)         { FactoryGirl.create(:site, name: "xyz") }
+    let!(:site_that_is_suffixed_with_match)  { FactoryGirl.create(:site, name: "abc@xyz") }
+    let!(:site_that_is_prefixed_with_match)  { FactoryGirl.create(:site, name: "xyz@abc") }
+    let!(:site_matching_with_different_case) { FactoryGirl.create(:site, name: "ABc@xYZ") }
+    let!(:site_that_doesnt_match)            { FactoryGirl.create(:site, name: "dong") }
+
+    it { should include(site_that_matches_exactly) }
+    it { should include(site_that_is_suffixed_with_match) }
+    it { should include(site_that_is_prefixed_with_match) }
+    it { should include(site_matching_with_different_case) }
+    it { should_not include(site_that_doesnt_match) }
+ end
+
+
 end
