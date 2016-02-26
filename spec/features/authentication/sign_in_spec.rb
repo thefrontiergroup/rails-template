9137679643a0ebaf849887d50c0b241042168614
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2016-02-26 10:40:40 +0800

    Add case for disabled user attempting to sign in

--- spec/features/authentication/sign_in_spec.rb
+++ spec/features/authentication/sign_in_spec.rb
@@ -23,6 +23,17 @@ feature 'A user can sign in' do
     end
   end
 
+  context "As a deleted User" do
+    let!(:user) { FactoryGirl.create(:user, :admin, deleted_at: 19.minutes.ago) }
+
+    scenario "Candidate signs in" do
+      user_fills_in_sign_in_form(user)
+      submit_form
+
+      expect(page).to have_flash :alert, /Invalid email or password./
+    end
+  end
+
   scenario "User attempts to sign in with invalid credentials" do
     submit_form
     expect(page).to have_flash :alert, /Invalid email or password./
@@ -30,9 +41,13 @@ feature 'A user can sign in' do
 
 private
 
-  def expect_sign_in_to_redirect_to(user, path)
+  def user_fills_in_sign_in_form(user)
     fill_in("Email", with: user.email)
     fill_in("Password", with: user.password)
+  end
+
+  def expect_sign_in_to_redirect_to(user, path)
+    user_fills_in_sign_in_form(user)
 
     submit_form
 
