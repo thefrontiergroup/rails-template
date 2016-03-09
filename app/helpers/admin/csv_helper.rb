1f5155fb73b2231b9fed898f7f99874e274d690f
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2016-03-09 17:01:47 +0800

    Remove CSV implementation

--- app/helpers/admin/csv_helper.rb
+++
@@ -1,7 +0,0 @@
-module Admin::CsvHelper
-
-  def link_to_csv(request, html_options={class: "btn"})
-    link_to("Download CSV", UriQueryMerger.new(request.url, {format: :csv}).merge, html_options)
-  end
-
-end
