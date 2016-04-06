0ebb9da0149ef55a1a6c448c14414ba7a27dc061
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2016-04-06 12:42:46 +0800

    Set delivery method per environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  # Email and Mailcatcher settings
+  # ActionMailer settings
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.raise_delivery_errors = true
--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,7 +1,6 @@
 options = YAML.load_file(Rails.root.join('config', 'smtp.yml'))[Rails.env]
 
 if options.present?
-  ActionMailer::Base.delivery_method = Rails.env.test? ? :test : :smtp
   ActionMailer::Base.smtp_settings = options
 elsif !Rails.env.test?
   raise "Please setup e-mail settings for #{Rails.env}"
