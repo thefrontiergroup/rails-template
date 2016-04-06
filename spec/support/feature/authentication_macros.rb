f93b28f303ac518929e9f4c746a8cc20d83d66ac
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2016-04-06 17:00:09 +0800

    Support adding traits and attributes

--- spec/support/feature/authentication_macros.rb
+++ spec/support/feature/authentication_macros.rb
@@ -10,9 +10,22 @@ module Feature
       end
     end
 
-    def sign_in_as(type_or_user)
+    # Use inside a feature, describe, or context block to create and sign a user in. EG:
+    #
+    # feature "an admin can acquire base" do
+    #
+    #   sign_in_as(:admin) # or
+    #   sign_in_as(:admin, :with_ten_legs) # or
+    #   sign_in_as(:admin, :with_ten_legs, {some_attribute: "has a value"})
+    #
+    #   scenario do
+    #     current_user.some_attribute # "has a value"
+    #   end
+    # end
+    #
+    def sign_in_as(user_type, *traits_and_attributes)
       before do
-        sign_in_as(type_or_user)
+        sign_in_as(user_type, *traits_and_attributes)
       end
 
       let(:current_user) { @current_user }
--- spec/support/feature/authentication_support.rb
+++ spec/support/feature/authentication_support.rb
@@ -1,16 +1,25 @@
 module Feature
   module AuthenticationSupport
 
-    def sign_in_as(role_or_user = :public_user)
+    # Use inside a feature spec to sign a user in.
+    #
+    # scenario "do a thing" do
+    #   sign_in_as(:admin) # or
+    #   sign_in_as(:admin, :with_ten_legs) # or
+    #   sign_in_as(:admin, :with_ten_legs, {some_attribute: "has a value"})
+    #
+    def sign_in_as(role_or_user, *traits_and_attributes)
       if role_or_user.is_a?(User)
         @current_user = role_or_user
       else
-        @current_user = FactoryGirl.create :user, role_or_user
+        @current_user = FactoryGirl.create :user, role_or_user, *traits_and_attributes
       end
 
       login_as(@current_user, scope: :user)
       visit get_dashboard_path(@current_user)
     end
+
+  private
 
     def get_dashboard_path(user)
       dashboard_policy = DashboardPolicy.new(user, :dashboard)
