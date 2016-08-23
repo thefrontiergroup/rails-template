59b8b59dfef100e1a2516662a432e69dadecb8db
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2016-08-23 17:51:58 +0800

    Fix #39: A user can close their account

---
+++ spec/features/admin/close_account_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'Admin can close their account' do
+
+  signed_in_as(:admin) do
+    before do
+      click_header_option("My Profile")
+    end
+
+    scenario do
+      click_link("Close my account")
+
+      expect(page).to have_flash(:notice, "Bye! Your account was successfully cancelled. We hope to see you again soon.")
+      expect(current_path).to eq(root_path)
+      expect(User.count).to eq(0)
+    end
+
+  end
+end
