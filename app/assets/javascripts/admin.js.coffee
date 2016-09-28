10020ef48553229a30fbddea43df4728a2e7b0e2
Patrik Affentranger <patrik.affentranger@gmail.com>
2016-09-28 13:31:53 +0800

    Update all admin views

--- app/assets/javascripts/admin.js.coffee
+++ app/assets/javascripts/admin.js.coffee
@@ -3,3 +3,7 @@
 #= require semantic-ui
 #= require jquery.timepicker.js
 #= require_tree ./admin
+
+$(document).ready ->
+  # create sidebar and attach to menu open
+  $('.ui.sidebar').sidebar('attach events', '.sidebar-toggle')
---
+++ app/helpers/inline_svg_helper.rb
@@ -0,0 +1,20 @@
+module InlineSvgHelper
+
+  # Inline SVG from @tomeara https://gist.github.com/tomeara/6515860
+  # Used to render SVG on a page
+  # = inline_svg('icons/svgdefs.svg')
+  def inline_svg(filename)
+    if Rails.application.config.assets.precompile
+      raw Rails.application.assets.find_asset(filename)
+    else
+      raw Rails.application.assets_manifest.find_sources(filename).try(:first)
+    end
+  end
+
+  # Use SVG internal link
+  # = svg_use('icon-user')
+  def svg_use(href, options = {})
+    content_tag(:svg, content_tag(:use, "", { "xlink:href": "##{href}" }), options)
+  end
+
+end
