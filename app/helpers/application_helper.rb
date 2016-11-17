7b1d88007f189d364a0d613e83004c262cfc6830
Patrik Affentranger <patrik.affentranger@gmail.com>
2016-11-17 23:09:04 +0800

    Introduce separate page title for admin section

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,13 @@
 module ApplicationHelper
 
   def current_page_title(object=nil)
-    page_title!(t('.title', label: object))
+    if can?(:manage, :all)
+      title_type = :admin
+    else
+      title_type = :default
+    end
+
+    page_title!(t('.title', label: object), title_type)
   end
 
 end
