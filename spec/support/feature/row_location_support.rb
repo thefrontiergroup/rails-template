09ec8bf2e074bdbfb69b8726ce792bca8fae7cfe
JK Gunnink <jk@thefrontiergroup.com.au>
2016-11-18 09:48:42 +0800

    Word choice

--- spec/support/feature/row_location_support.rb
+++ spec/support/feature/row_location_support.rb
@@ -4,7 +4,7 @@ module Feature
     # feature tests.
 
     # It expects to receive an integer for the row number the test should check
-    # as well as an optional expectaction for what is to occur within that row.
+    # as well as an optional block for what is to occur within that row.
     def in_table_row_number(row_number)
       within("tr:nth-child(#{row_number})") { yield }
     end
